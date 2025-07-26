cf7b3c1ceb375283f89631a261b623ff0bac50a0
YashviKalathiya17 <yashvikalathiya402@gmail.com>
2025-07-26 17:05:50 +0530

    fsMethod

--- Basics/index.js
+++ Basics/index.js
@@ -1,16 +1,30 @@
-// Create a basic server
-const { log } = require("console");
-const http = require("http");
+// // Create a basic server
+// const { log } = require("console");
+// const http = require("http");
 
-const port = 5050;
+// const port = 5050;
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
-    res.end('Hello world!!');
+// const server = http.createServer((req, res) => {
+//     res.writeHead(200, {
+//         'Content-Type': 'text/plain'
+//     });
+//     res.end('Hello world!!');
+// })
+
+// server.listen(port, () => {
+//     console.log(`server running on port ${port}`);
+// })
+
+// Another Way:---
+
+const http = require('http')
+
+const port = 6060
+
+const server=   http.createServer((req,res)=>{
+    res.end("Hello User! welcome to the Node");
 })
 
-server.listen(port, () => {
-    console.log(`server running on port ${port}`);
+server.listen(port,(err)=>{
+    !err?console.log(`server running on port ${port}`):null
 })

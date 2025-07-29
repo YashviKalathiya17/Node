9b3058dc5d12972a60ef89b5b26f08d0b2c49bfc
YashviKalathiya17 <yashvikalathiya402@gmail.com>
2025-07-29 21:26:58 +0530

    "Pug&HandleBars"

---
+++ Basics/Pug/index.js
@@ -0,0 +1,21 @@
+// Pug-- Pug is a template engine for Node.js — it helps you write HTML faster and 
+// cleaner using indentation instead of brackets and closing tags.
+
+
+// {/* <p>Timothy's Pug source code!</p> */}
+// h1 Hello #{name}
+
+// const express = require('express');
+//const app = express();
+
+//app.set('view engine', 'ejs');
+
+
+// HandleBars--Handlebars is a logic-less templating engine for JavaScript.
+//  It lets you generate dynamic HTML by combining templates with data.
+
+// {{#if loggedIn}}
+//   <p>Welcome back!</p>
+// {{else}}
+//   <p>Please log in</p>
+// {{/if}}

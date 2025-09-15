382f6a7ef369b804d805feb9fe1da6e0afa0c0ee
YashviKalathiya17 <yashvikalathiya402@gmail.com>
2025-09-15 20:43:50 +0530

    MovieProject

---
+++ SetUp/index.js
@@ -0,0 +1,19 @@
+import { Age } from "./second.js";
+
+// console.log("Hello World");
+// const pi=3.14;
+
+// console.log(pi);
+
+// console.log(pi*2);
+
+// --> Common JS Module System  
+// const yk = require("./second");
+// console.log(yk);
+
+// --> ES6 Module System
+
+
+// you have to do these:---      
+// import { Age } from "./second.js";
+Age()
---
+++ SetUp/second.js
@@ -0,0 +1,14 @@
+// const { name } = require("ejs");
+
+// const Yashvi={
+//     name:"Yashvi",
+//     age:54,
+//     city:"Surat"
+// }
+// module.exports=Yashvi;
+
+
+export function Age() {
+    console.log("my age is 54");
+
+}
